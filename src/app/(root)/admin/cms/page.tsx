a0c06862755ccca2240c1bce3196d23811b01fb0
rishabh.b <rishabh.b@mordorintelligence.com>
2025-02-20 15:19:32 +0530

    Add CMS page and component for admin interface

---
+++ src/app/(root)/admin/cms/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Cms from '../../../components/admin/Cms'
+function Cmspage() {
+  return (
+    <Cms/>
+  )
+}
+
+export default Cmspage
---
+++ src/app/components/admin/Cms/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Cms() {
+  return (
+    <div>Cmspage</div>
+  )
+}
+
+export default Cms
